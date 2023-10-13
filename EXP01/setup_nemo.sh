b50961d33d533cdbd0fa678399243c91fb06f2ca
jenniferagraham <jennifer.graham@cefas.co.uk>
2023-10-13 16:28:19 +0100

    Set paths to forcing files

---
+++ EXP01/setup_nemo.sh
@@ -0,0 +1,4 @@
+ln -s /gpfs/data/cefas-shared/NEMO/jmmp/AMM15/FORCING/RIVERS/Spread_Loirre_Garron_N_G.nc ./rivers_y2004.nc
+ln -s /gpfs/data/cefas-shared/NEMO/jmmp/AMM15/COORDINATES/CO9_rimwidth_9_coordinates.bdy_baltic.nc .
+ln -s /gpfs/data/cefas-shared/NEMO/jmmp/AMM15/COORDINATES/amm15.bdy.coordinates.rim15.nc .
+ln -s /gpfs/data/cefas-shared/NEMO/jmmp/AMM15/FORCING/ .
