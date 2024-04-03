630a25b1d8656cf54bf047c71b6cc53d6a092261
jenniferagraham <jennifer.graham@cefas.co.uk>
2024-04-03 15:28:21 +0100

    Update paths to forcing files

--- EXP4.0.4/setup_nemo.sh
+++ EXP4.0.4/setup_nemo.sh
@@ -1,4 +1,16 @@
-ln -s /gpfs/data/cefas-shared/NEMO/jmmp/AMM15/FORCING/RIVERS/Spread_Loirre_Garron_N_G.nc ./rivers_y2004.nc
-ln -s /gpfs/data/cefas-shared/NEMO/jmmp/AMM15/COORDINATES/CO9_rimwidth_9_coordinates.bdy_baltic.nc .
-ln -s /gpfs/data/cefas-shared/NEMO/jmmp/AMM15/COORDINATES/amm15.bdy.coordinates.rim15.nc .
-ln -s /gpfs/data/cefas-shared/NEMO/jmmp/AMM15/FORCING/ .
+# set input paths to those provided here: https://github.com/CefasRepRes/CO_AMM15
+
+INPUT=/gpfs/data/cefas-shared/NEMO/jmmp/AMM15/
+
+ln -s ${INPUT}/COORDINATES/CO9_rimwidth_9_coordinates.bdy_baltic.nc .
+ln -s ${INPUT}/COORDINATES/amm15.bdy.coordinates.rim15.nc .
+ln -s ${INPUT}/DOMAIN_CFG/GEG_SF12.nc .
+ln -s ${INPUT}/RESTARTS/P15b/ini_GEG_NICOBALS.nc .
+ln -s ${INPUT}/RESTARTS/P15b/ini_GEG_NICOBALT.nc .
+ln -s ${INPUT}/FORCING/RIVERS/ ./RIV
+ln -s ${INPUT}/FORCING/BDY/ .
+ln -s ${INPUT}/FORCING/SBC/ .
+ln -s ${INPUT}/FORCING/TIDES/ .
+
+mkdir RESTARTS
+
